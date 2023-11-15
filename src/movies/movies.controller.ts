3c1eecbdc622854640764d759cb44941b517bcf1
JaehyunWoo <jaehyunwoo@JaehyunWoos-MacBook-Air.local>
2023-11-15 16:15:06 +0900

    initialize controller & service

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/Movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {
+
+    }
+
+    @Get()
+    getAll(): Movie[] {
+        return this.moviesService.getAll()
+    }
+    
+    @Get(":id")
+    getOone(@Param("id") movieId: string) : Movie {
+        return this.moviesService.getOne(movieId)
+    }
+    
+    @Post()
+    create(@Body() movieData) {
+        console.log(movieData)
+        return movieData
+    }
+    
+    @Delete(":id") 
+    remove(@Param("id") movieId:string,) {
+        return this.moviesService.deleteOne(movieId)
+    }
+    
+    @Patch('/:id')
+    path(@Param('id') movieId:string, @Body() updateData) {
+        return {
+            updatedMovie: movieId,
+            ...updateData
+        }
+    }
+    
+    @Get("search")
+    search(@Query("year") searchingYear : string) {
+        return `We are searching for a movie made after ${searchingYear}`
+    }
+    
+}
